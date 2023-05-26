95364f37a3176437353f965f058bc3f5974372e4
JhosselynGonzalezSanchez7 <l19350296@tuxtepec.tecnm.mx>
2023-05-25 18:24:50 -0600

    frockend utilizando  apis

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Categories from './components/Categories'
+import GifsExpo from './components/GifsExpo'
+
+function App() {
+  const [categories, setCategories] = useState(["Paisajes", "Girasoles"])
+
+  return (
+    <div className='m-5'>
+      <h3>Gif Demo App</h3>
+      <hr/>
+      <Categories 
+      categories={categories} 
+      setCategories={setCategories} 
+      />
+      <hr />
+      <GifsExpo 
+      categories={categories} 
+      setCategories={setCategories}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/GifsExpo/index.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+
+const GifsExpo = ({categories = []}) => {
+    const [urlList, setUrlList] = useState([])
+
+    const getGifs = async (categories) => {
+        if (categories.length === 0) {
+            setUrlList([])
+            return
+        }
+
+        const responsesList = await Promise.all(categories.map(async (category) => {
+            const response = await fetch(
+                `https://api.giphy.com/v1/gifs/search?api_key=S58JR00FCbGBp0ZN2IVDz2Xc9LaXmODm&q=${category}`
+            )
+            const apiResponse = await response.json()
+            return apiResponse.data
+        }))
+
+        let gifsList = []
+
+        responsesList.forEach((data) => {
+            data.forEach((item) => {
+                gifsList = [...gifsList, item.images.fixed_height.url.split('?')[0]]
+            })
+        })
+
+        setUrlList([...gifsList])
+    }
+    
+    useEffect(() => {
+        //Efecto
+        getGifs(categories)  
+        },
+        //arreglo de dependencias
+        [categories]
+    )
+   
+    return (
+        <>
+            <div>
+                {
+                    urlList.map((url) => {
+                        return (
+                            <img key={url} src={url} />
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default GifsExpo
